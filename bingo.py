326b2bce3bbb64531cc60cb640abacc4fcc6e48b
Pedro Schuller <pedro.schuller95@gmail.com>
2025-01-27 23:23:58 +0000

    Update bingo.py

--- bingo.py
+++ bingo.py
@@ -23,22 +23,23 @@ def check_bingo(checked):
     return ""
 
 def main():
-    st.title("Bingo da VIII Convenção Nacional")
+    st.title("Bingo da IX Convenção Nacional")
 
-    words = ["Inerências", "Verdadeiramente liberal", "Burocracia",
+    words = ["Inerências", "Verdadeiramente liberal", "Burocracia", "Isso é socialista!",
              "Comité central", "Transparência", "Eu não sou jurista, mas...",
-             "Delegados", "Separação de poderes", "José Cardoso",
-             "Babuje", "RGPD", "Subscritores", "Distritais",
-             "'Jota' ou 'Juventude' da IL", "Portal do Membro", "Órgão Máximo",
-             "O partido é como uma empresa", "O partido é como o estado",
-             "Salários", "Partilha de contactos", "Co-criação", "Realmente liberal",
-             "Quórum", "Tens de terminar", "Ratificar", "Quotas",
-             "Observador", "Alguém no zoom não aparece", "Viva Portugal, Viva a Iniciativa Liberal",
-             "Leva a bebida para o púlpito", "Muito bem!", "Pin arco íris",
-             "Estou no partido há pouco tempo", "O partido mudou",
-             "Apupos", "Eu não ia falar...", "Problemas de som", "Está tudo a pensar no almoço/jantar",
+             "Delegados", "Separação de poderes", "José Cardoso", "Encostem-me à parede!",
+             "Malas", "Distritais", "Empoderar o Membro", "Microfone dá feedback",
+             "'Jota' da IL", "Portal do Membro", "Órgão Máximo", "Assinaturas", "Freguesias",
+             "O partido é como uma empresa", "O partido é como o estado", "Carlos Moedas",
+             "Salários", "Nepotismo", "Realmente liberal", "Elon Musk", "Fechem as portas!"
+             "Quórum", "Tem de terminar...", "Motoserra", "AFUERA!", "15%", "Unir o partido",
+             "Observador", "Alguém no zoom não aparece", "Wokismo", "Aplauso do aviso para almoço/jantar"
+             "Leva a bebida para o púlpito", "Pin arco íris", "Introduzir o membro", 
+             "Estou no partido há pouco tempo", "O partido mudou!", "Liberal em toda a linha",
+             "Apupos", "Eu não ia falar, mas...", "Problemas de som", "Está tudo a pensar no almoço/jantar",
              "Melancia azul", "Liberalómetro", "WOKE", "Estou mesmo a terminar...", "BOM DIA LIBERAIS",
-             "Referência ao jogo da seleção", "3 minutos não chega..."]
+              "3 minutos não chega...", "Camaradas...", "Unidos AO Liberalismo", "Tantas listas!",
+              "Não sou candidato/a...", "Javier Milei", "Assino por baixo", "Autonomia dos núcleos"]
 
     seed = st.number_input("Insere o teu número de membro:", min_value=1, max_value=9000, value=1)
     
